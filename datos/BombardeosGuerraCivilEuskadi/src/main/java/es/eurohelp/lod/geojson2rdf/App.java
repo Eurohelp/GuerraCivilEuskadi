c8cde5768441a1f2019b529b124418eb386c2c55
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2018-05-28 16:31:44 +0200

    GeoJSON2RDF

--- datos/BombardeosGuerraCivilEuskadi/src/main/java/es/eurohelp/lod/geojson2rdf/App.java
+++ datos/BombardeosGuerraCivilEuskadi/src/main/java/es/eurohelp/lod/geojson2rdf/App.java
@@ -3,7 +3,10 @@ package es.eurohelp.lod.geojson2rdf;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -13,6 +16,10 @@ public class App {
 	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
       JSONParser parser = new JSONParser();
       JSONObject obj = (JSONObject) parser.parse(new FileReader("bombardeos-guerra-civil.geojson"));
-      System.out.println(obj.get("type"));
+      JSONArray features = (JSONArray) obj.get("features");
+      Iterator<JSONObject> iterator = features.iterator();
+      while (iterator.hasNext()) {
+          System.out.println((iterator.next()).get("id"));
+      }
 	}
 }
