9036191b28508e95cb7cc8132369fe26af4c2317
Jonan Oribe <disaicold@gmail.com>
2018-06-21 10:34:57 +0200

    Bug Timeline fixed

--- front-end/js/timeline.js
+++ front-end/js/timeline.js
@@ -3,64 +3,6 @@ var data = [];
 var datos = [];
 var a = new Map();
 
-function regexCriba(busqueda, texto) {
-    const regex = /(busqueda)/gm;
-    const str = String(texto);
-    let m;
-
-    while ((m = regex.exec(str)) !== null) {
-        // This is necessary to avoid infinite loops with zero-width matches
-        if (m.index === regex.lastIndex) {
-            regex.lastIndex++;
-        }
-
-        // The result can be accessed through the `m`-variable.
-        m.forEach((match, groupIndex) => {
-            console.log(`Found match, group ${groupIndex}: ${match}`);
-        });
-        return true;
-    }
-}
-
-var diccionarioCriba = [
-    "http://id.euskadi.eus/public-sector/history/missing-person/Arce-Saez-Acisdo-Abanto-Zierbena-Asturias", "http://id.euskadi.eus/public-sector/history/missing-person/Arizmendi-Aizpurua-Jesus-Azkoitia-Asturias",
-    "http://id.euskadi.eus/public-sector/history/missing-person/Blanco-Perez-Abelardo-Sestao-Asturias", "http://id.euskadi.eus/public-sector/history/missing-person/Camiruaga-Lotina-Agustin-Erandio-Asturias",
-    "http://id.euskadi.eus/public-sector/history/missing-person/Domingo-Beriain-Augusto-Hernani-Asturias", "http://id.euskadi.eus/public-sector/history/missing-person/Elgarresta-Zufiaurre-Jose-Antonio-Urretxu-Asturias",
-    "http://id.euskadi.eus/public-sector/history/missing-person/Fernandez-Arroyabe-Eduardo-Zumaia-Asturias", "http://id.euskadi.eus/public-sector/history/missing-person/Flores-Echevarria-Ricardo-Sestao-Asturias",
-    "http://id.euskadi.eus/public-sector/history/missing-person/Flores-Palenque-Pedro-Ci%C3%A9rvana-Asturias", "http://id.euskadi.eus/public-sector/history/missing-person/Gallo-Ruiz-Ezequiel-Sestao-Asturias",
-    "http://id.euskadi.eus/public-sector/history/missing-person/Garate-Unanue-Manuel-Azkoitia-Asturias", "http://id.euskadi.eus/public-sector/history/missing-person/Gomez-German-Pedro-Basauri-Asturias",
-    "http://id.euskadi.eus/public-sector/history/missing-person/Iturbe-Uribe-Martin-Arrasate-Asturias", "http://id.euskadi.eus/public-sector/history/missing-person/Leguina-Gorostiza-Jose-Basauri-Asturias",
-    "http://id.euskadi.eus/public-sector/history/missing-person/Mendizabal-Sarasola-Antonio-Donostia-San-Sebasti%C3%A1n-Asturias", "http://id.euskadi.eus/public-sector/history/missing-person/Michelena-Arizmendiarreta-Juan-Eibar-Asturias",
-    "http://id.euskadi.eus/public-sector/history/missing-person/Olabarrieta-Mendibil-Serafin-Barakaldo-Asturias", "http://id.euskadi.eus/public-sector/history/missing-person/Orive-Gomez-Arsenio-Bilbo-Asturias",
-    "http://id.euskadi.eus/public-sector/history/missing-person/Pintor-Carrera-Martin-Donostia-San-Sebasti%C3%A1n-Asturias", "http://id.euskadi.eus/public-sector/history/missing-person/Rodriguez-Churruca-Felix-Bilbo-Asturias",
-    "http://id.euskadi.eus/public-sector/history/missing-person/Santiago-Gomez-Benito-Getxo-Otxandio", "http://id.euskadi.eus/public-sector/history/missing-person/Santiago-Gorriz-Benito-Getxo-Zornotza",
-    "http://id.euskadi.eus/public-sector/history/missing-person/Pedrosa-Valiente-Nicasio-Hernani-Irun", "http://id.euskadi.eus/public-sector/history/missing-person/Salaverria-Izaguirre-Eugenia-Donostia-San-Sebasti%C3%A1n-Donostia-San-Sebasti%C3%A1n",
-    "http://id.euskadi.eus/public-sector/history/missing-person/Tasende-Del-Coro-Silverio-Bilbo-Ubidea", "http://id.euskadi.eus/public-sector/history/missing-person/Zuazo-Urquiaga-Aniceto-Bilbo-Bilbo",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/orden/bopv193701266/es_def/index.shtml", "http://id.euskadi.eus/public-sector/history/missing-person/Royos-Aguirre-Alfredo-Basauri-Zornotza",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/orden/bopv193701265/es_def/index.shtml",
-    "http://id.euskadi.eus/public-sector/history/bombardment/m%C3%BAgica-30-04-1937-02-05-1937",
-    "http://id.euskadi.eus/public-sector/history/bombardment/artzentales--22-06-1937-02-07-1937",
-    "http://id.euskadi.eus/public-sector/history/bombardment/karrantza--17-06-1937-02-07-1937",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/orden/bopv193700978/es_def/index.shtml",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/orden/bopv193701113/es_def/index.shtml",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/orden/bopv193701112/es_def/index.shtml",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/orden/bopv193701103/es_def/index.shtml",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/decreto/bopv193701102/es_def/index.shtml",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/orden/bopv193701106/es_def/index.shtml",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/orden/bopv193701107/es_def/index.shtml",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/orden/bopv193701108/es_def/index.shtml",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/orden/bopv193701109/es_def/index.shtml",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/orden/bopv193701110/es_def/index.shtml",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/orden/bopv193701111/es_def/index.shtml",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/orden/bopv193701114/es_def/index.shtml",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/orden/bopv193701115/es_def/index.shtml",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/orden/bopv193701104/es_def/index.shtml",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/orden/bopv193701105/es_def/index.shtml",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/orden/bopv193701116/es_def/index.shtml",
-    "http://www.legegunea.euskadi.eus/x59-contfich/eu/contenidos/orden/bopv193701117/es_def/index.shtml",
-    "http://id.euskadi.eus/public-sector/history/bombardment/m%C3%BAgica-30-04-1937-02-05-1937"
-];
-
 function generarTimeline() {
 
     repeticionesFecha = 1;
@@ -98,9 +40,7 @@ function generarTimeline() {
 
             UriBombardeoURL = '<a href=' + $(element).find("binding[name='resource']").find("uri").text() + ' target="_blank">' + $(element).find("binding[name='resource']").find("uri").text() + '</a>';
             var FechaBombardeo = "";
-            var itemp = diccionarioCriba.includes(UriBombardeoURL)
             var uriTemp = $(element).find("binding[name='resource']").find("uri").text();
-            var indTemp = regexCriba(uriTemp, diccionarioCriba);
             if (UriBombardeoURL.includes("missing-person")) {
                 FechaBombardeo = $(element).find("binding[name='date']").find("literal").text() + "||http://id.euskadi.eus/def/euskadipedia/missing-person";
             } else if (UriBombardeoURL.includes("dof")) {
@@ -108,28 +48,23 @@ function generarTimeline() {
             } else if (UriBombardeoURL.includes("bombardment")) {
                 FechaBombardeo = $(element).find("binding[name='date']").find("literal").text() + "||http://dbpedia.org/resource/Aerial_bombing_of_cities";
             }
-            var dicTemp = diccionarioCriba.includes(UriBombardeoURL);
-            if (diccionarioCriba.includes(UriBombardeoURL) == false) {
-                if (FechaBombardeo != "") {
-                    var temp = FechaBombardeo.split("||");
-                    var tempCriba = temp[0];
-                    var fechaTempCriba = tempCriba.split("-");
-                    var dateTemp = new Date(Number(fechaTempCriba[0]), Number(fechaTempCriba[1] - 1), Number(fechaTempCriba[2]));
-                    var fechaMinima = new Date(1936, 6, 18);
-                    var fechaMaxima = new Date(1937, 2, 1);
+            if (FechaBombardeo != "") {
+                var temp = FechaBombardeo.split("||");
+                var tempCriba = temp[0];
+                var fechaTempCriba = tempCriba.split("-");
+                var dateTemp = new Date(Number(fechaTempCriba[0]), Number(fechaTempCriba[1] - 1), Number(fechaTempCriba[2]));
+                var fechaMinima = new Date(1936, 6, 18);
+                var fechaMaxima = new Date(1937, 2, 1);
 
-                    if (!fechasDOF.includes(FechaBombardeo) && dateTemp > fechaMinima && dateTemp < fechaMaxima) {
-                        fechasDOF.push(FechaBombardeo);
-                        FechasHashMap.set(FechaBombardeo, repeticionesFecha);
-                    } else if (fechasDOF.includes(FechaBombardeo) && dateTemp > fechaMinima && dateTemp < fechaMaxima) {
-                        temporalRepeticiones = FechasHashMap.get(FechaBombardeo);
-                        temporalRepeticiones++;
-                        FechasHashMap.set(FechaBombardeo, temporalRepeticiones);
-                    }
-
+                if (!fechasDOF.includes(FechaBombardeo) && dateTemp > fechaMinima && dateTemp < fechaMaxima) {
+                    fechasDOF.push(FechaBombardeo);
+                    FechasHashMap.set(FechaBombardeo, repeticionesFecha);
+                } else if (fechasDOF.includes(FechaBombardeo) && dateTemp > fechaMinima && dateTemp < fechaMaxima) {
+                    temporalRepeticiones = FechasHashMap.get(FechaBombardeo);
+                    temporalRepeticiones++;
+                    FechasHashMap.set(FechaBombardeo, temporalRepeticiones);
                 }
-            } else if (diccionarioCriba.includes(UriBombardeoURL) == true) {
-                console.log("EO");
+
             }
 
         });
@@ -164,12 +99,12 @@ function generarTimeline() {
 
         var options = {
 
-            height: '750px',
+            height: '650px',
             min: new Date(1936, 6, 1), // lower limit of visible range
-            max: new Date(1937, 9, 18), // upper limit of visible range
+            max: new Date(1937, 4, 18), // upper limit of visible range
             start: new Date(1936, 8, 1),
             zoomMin: 1000 * 60 * 60 * 24 * 7, // one week in milliseconds
-            zoomMax: 1000 * 60 * 60 * 24 * 31 * 3 // about three months in milliseconds
+            zoomMax: 1000 * 60 * 60 * 24 * 31 * 3.3 // about three months in milliseconds
 
         };
 
